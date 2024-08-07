e8c0f751656ea2892046ade2ce64319e9d873f3d
Telkar, Namith Manjunath <namithtelkar@gmail.com>
2024-08-06 19:24:58 -0500

    Learnt about declaring int, float and string

--- main.go
+++ main.go
@@ -3,5 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// var nameOne string = "Jon"
+	// var nameTwo = "Arya"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameThree = "Sansa"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "Bran"
+
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// var numbOne int8 = 25
+	// var numbTwo int8 = -128
+	// var numbThree uint8 = 255
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 3567465754.98
+	scroreThree := 25.98
+
+	fmt.Println(scoreOne, scoreTwo, scroreThree)
+
+
 }
